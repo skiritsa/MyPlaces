96e732f509694073e44fbb2fcf77b08072c8fdc3
alexkiritsa <skiritsa@gmail.com>
2019-11-08 16:57:50 +0200

    Added MapVC

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Alex Kiritsa on 08.11.2019.
+//  Copyright © 2019 Alex Kiritsa. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closeVS() {
+        dismiss(animated: true)
+    }
+}
